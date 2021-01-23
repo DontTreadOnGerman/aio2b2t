f4550f7dd27640e52ff07a23b8857d379e2c6b55
German2k2k <38298732+DontTreadOnGerman@users.noreply.github.com>
2021-01-23 00:24:04 -0500

    fucked up again

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setuptools.setup(
     description="aio2b2t is a modern, async, API wrapper for https://2b2t.dev and https://2b2t.io.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/DontTreadOnGerman/",
+    install_requires=['aiohttp'],
+    url="https://github.com/DontTreadOnGerman/aio2b2t",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
